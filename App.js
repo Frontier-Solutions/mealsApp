b0f9df823e0f7c5c01e6caffcb3d643df85bb0bf
rick <rg.henriques@hotmail.com>
2023-12-02 17:11:30 +0000

    displaying categories, no styling

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import CategoriesScreen from "./screens/CategoriesScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello World!!!!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    //   <Text style={styles.text}>Hello World!!!!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+
+    <CategoriesScreen />
   );
 }
 
