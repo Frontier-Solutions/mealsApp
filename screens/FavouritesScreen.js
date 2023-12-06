363b73921c83bf346f809d755c3501d9e97a9746
rick <rg.henriques@hotmail.com>
2023-12-06 18:09:48 +0000

    no favourites message

--- screens/FavouritesScreen.js
+++ screens/FavouritesScreen.js
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { Text, View, StyleSheet } from "react-native";
 import MealsList from "../components/MealsList/MealsList";
 import { FavouritesContext } from "../store/context/favourites-context";
 import { MEALS } from "../data/dummy-data";
@@ -9,7 +10,28 @@ function FavouritesScreen() {
     favouriteMealsCtx.ids.includes(meal.id)
   );
 
+  if (favouriteMeals.length === 0) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>You have no favourites yet</Text>
+      </View>
+    );
+  }
+
   return <MealsList items={favouriteMeals} />;
 }
 
 export default FavouritesScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
